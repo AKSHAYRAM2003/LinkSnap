bba3f24a07b096661126afee182e19aa2aa77b13
Akshay  H R <ramakshay100@gmail.com>
2025-03-13 17:09:00 +0530

    admin validations added with correct models

--- django_project/bookmarks/admin.py
+++ django_project/bookmarks/admin.py
@@ -1,3 +1,18 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Bookmark, Tag
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    search_fields = ('name',)
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Bookmark)
+class BookmarkAdmin(admin.ModelAdmin):
+    list_display = ('title', 'url', 'user', 'is_favorite', 'created_at')
+    list_filter = ('is_favorite', 'created_at', 'tags')
+    search_fields = ('title', 'description', 'url')
+    readonly_fields = ('created_at', 'updated_at')
+    filter_horizontal = ('tags',)
+    date_hierarchy = 'created_at'
